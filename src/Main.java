d42da869ea0f41604b77690ea8a2ebadd8cda1cc
Andrew2380 <andrey238054@gmail.com>
2020-11-01 20:24:09 +0300

    Get a ruble two

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@ public class Main {
     public static void main(String[] args) {
         boolean user = true;
         int persent =100;
-        long WasAmountKopecks = 10000;
-        long ReplenishedAmountKopecks = 110000;
-        long Bonus = (ReplenishedAmountKopecks / persent) / 100;
-        long totalamount = (ReplenishedAmountKopecks + WasAmountKopecks) / 100;
+        long wasamount = 100;
+        long replenishedamount = 1101;
+        long bonus = (replenishedamount / persent);
+        long totalamount = (replenishedamount + wasamount);
         long limit = 0;
-        if (ReplenishedAmountKopecks <= 100000){
-            Bonus = limit;
+        if (replenishedamount <= 1000){
+            bonus = limit;
     }
-        System.out.println(totalamount + Bonus );
-        System.out.println(Bonus);
+        System.out.println(totalamount + bonus );
+        System.out.println(bonus);
     }
 }
